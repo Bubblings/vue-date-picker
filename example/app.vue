8c731f0d992cdaa8325bd9e3e2519c57b5b9c63a
hawjehlee <hawjeh.lee@websparks.sg>
2019-10-24 12:33:14 +0800

    - added moment to govern the date format - v-model to support type in date

--- example/app.vue
+++ example/app.vue
@@ -7,22 +7,29 @@
 <template>
   <div class="demo">
     <datepicker :readonly="true" format="YYYY-MM-DD" name="date1"></datepicker>
-    <datepicker value="2018-9-5" format="YYYY-M-D" name="date2" :input-attr="{ 'data-test': 'value' }"></datepicker>
+    <datepicker v-model="date2" format="YYYY-M-D" name="date2" :input-attr="{ 'data-test': 'value' }"></datepicker>
     <datepicker :readonly="true" format="MMM/D/YYYY" name="date3" :disabled-date="disabledDate"></datepicker>
   </div>
 </template>
 
 <script>
-import datepicker from '../src/vue-date-picker.vue'
+import datepicker from '../src/vue-date-picker.vue';
 
 export default {
   components: {
     datepicker
   },
+  data() {
+    return {
+      date1: '',
+      date2: '2018-9-5',
+      date3: ''
+    };
+  },
   methods: {
-    disabledDate (date) {
-      return date.getTime() < Date.now()
+    disabledDate(date) {
+      return date.getTime() < Date.now();
     }
   }
-}
+};
 </script>
